d2e2d37622adeb978e572c6958a6066c96e8f49b
scottbedard <scottbedard90@gmail.com>
2016-02-24 00:21:56 -0700

    Fix nav reference to shared state

--- src/state/nav.js
+++ src/state/nav.js
@@ -16,7 +16,7 @@ module.exports = {
      * @return {void}
      */
     close() {
-        this.isExpanded = false;
+        this.state.isExpanded = false;
     },
 
     /**
@@ -25,7 +25,7 @@ module.exports = {
      * @return {void}
      */
     open() {
-        this.isExpanded = true;
+        this.state.isExpanded = true;
     },
 
     /**
