c602cb4a1f51cb3a3917d7d20bd0113c6b92304c
scottbedard <scottbedard90@gmail.com>
2016-02-24 22:45:02 -0700

    Begin work on blog index

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,15 +1,66 @@
 <style lang="sass" scoped> @import 'core';
-
+    .header {
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
 
 <template>
     <main class="page">
-        <h1>Hello from the blog page</h1>
+        <div class="header">
+            <h1>Blog</h1>
+            <div>
+                <input type="search" v-model="search" placeholder="Search the blog...">
+            </div>
+        </div>
+        <ul>
+            <li v-for="post in posts"><pre>{{ post | json }}</pre></li>
+        </ul>
     </main>
 </template>
 
 <script>
-module.exports = {
+    import BlogResource from 'resources/blog';
 
-};
+    module.exports = {
+
+        /**
+         * @return {Object}
+         */
+        data() {
+            return {
+                search: '',
+            };
+        },
+
+        /**
+         * @type {Object}
+         */
+        route: {
+            /**
+             * @type {Boolean}
+             */
+            waitForData: true,
+
+            /**
+             * Fetch route data
+             *
+             * @param  {Object}     transition
+             * @return {Promise}
+             */
+            data(transition) {
+                return BlogResource.get().then(response => this.$set('posts', response.data));
+            },
+        },
+
+        /**
+         * @return {Object}
+         */
+        data() {
+            return {
+                posts: [],
+            };
+        },
+    };
 </script>
