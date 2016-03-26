8fc0a73237cc3c3c0ee00520404fdc2d09fa0430
scottbedard <scottbedard90@gmail.com>
2016-03-26 16:40:10 -0700

    Remove explicit http reference

--- src/pages/blog/components/disqus.vue
+++ src/pages/blog/components/disqus.vue
@@ -37,7 +37,7 @@
                     let dsq = document.createElement('script');
                     dsq.type = 'text/javascript';
                     dsq.async = true;
-                    dsq.src = 'http://beeasyboards.disqus.com/embed.js';
+                    dsq.src = '//beeasyboards.disqus.com/embed.js';
                     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
                     disqusIsFetched = true;
                 }
